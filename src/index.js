750ed502a077a53d83718c57f2d07ad7b4707022
David Stockdale <dstock3@gmail.com>
2021-08-25 17:54:10 -0500

    Added test image along with corresponding configuration specs.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './style.css';
+import test from './test.jpg';
 function component() {
     const element = document.createElement('div');
     element.textContent = "Hello"
     element.classList.add('hello');
+
+    const myImage = new Image();
+    myImage.src = test;
+
+    element.appendChild(myImage)
   
     return element;
   }
