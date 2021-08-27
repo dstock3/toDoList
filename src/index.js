092acb01de739c0a9124acb0f3e6ac30db16691a
David Stockdale <dstock3@gmail.com>
2021-08-27 17:57:30 -0500

    Built second toDo object and skeleton for a function that loops through all objects and displays them on the page.

--- src/index.js
+++ src/index.js
@@ -7,5 +7,34 @@ const toDoItem = toDo(
   "Create an application to manage daily tasks.",
   "9/15/2021",
   "1", 
-  "This the first object created using the toDo factory function."
+  "This the first object created using the toDo factory function.",
 )
+
+const changeOil = toDo(
+  "Time for an Oil Change!",
+  "Change the oil in your beautiful Mazda 2!",
+  "9/28/2021",
+  "3",
+  "test note",
+)
+
+const toDoList = [toDoItem, changeOil]
+
+function elementBuilder(element, classLabel, parentName) {
+  let item = document.createElement(element);
+  item.classList.add(classLabel);
+  parentName.appendChild(item);
+  return item;
+}
+
+const toDoBuilder = (toDoArray) => {
+  for (let i = 0; i < toDoArray.length; i++) {
+    console.log(toDoArray[i].title);
+    console.log(toDoArray[i].description);
+    console.log(toDoArray[i].dueDate);
+    console.log(toDoArray[i].priority);
+    console.log(toDoArray[i].notes);
+  };
+};
+
+toDoBuilder(toDoList)
