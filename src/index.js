94ad41d4a615cbf001597e84ddaf09e729c8cd61
David Stockdale <dstock3@gmail.com>
2021-08-26 21:57:49 -0500

    Added HtmlWebpackPlugin and corresponding config. Cleaned dist folder.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-    element.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    const btn = document.createElement('button');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
 
     return element;
