87f4110dfcf86e2b6f631a4a945e5be7759551e6
David Stockdale <dstock3@gmail.com>
2021-09-03 17:29:04 -0500

    Removed projectDiv assignment in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import './style.css';
 import { project, projectTracker } from './task.js'
-import { projectSize, projectBuilder, applyButtons } from'./projectElements.js'
+import { projectBuilder, applyButtons } from'./projectElements.js'
 
 const start = (() => {
   let projects = projectTracker()
@@ -9,8 +9,6 @@ const start = (() => {
   const defaultProjectSet = projectBuilder(defaultProject);
   projects.masterList.push(defaultProjectSet);
   applyButtons(projects.masterList);
-  let projectDiv = document.getElementsByClassName("project-container")[0];
-  projectSize(projectDiv)
 
   return { projects }
 })();
