ea91a864ded564a443d59fd91148ca0f182d4677
David Stockdale <dstock3@gmail.com>
2021-09-06 14:43:22 -0500

    Created a container to store the top elements of the sidebar. Adjusted other elements accordingly.

--- src/sidebar.js
+++ src/sidebar.js
@@ -3,26 +3,27 @@ import { today } from './date'
 
 const sidebar = (() => {
     const element = elementBuilder("div", "sidebar", body);
-    const head = elementBuilder("h1", "sidebar-head", element);
+
+    const headContainer = elementBuilder("div", "head-container", element);
+    const head = elementBuilder("h1", "sidebar-head", headContainer);
     head.textContent = "OnTask";
-    const sideButtons = elementBuilder("div", "side-button-container", element)
+
+    const sideButtons = elementBuilder("div", "side-button-container", headContainer)
   
     const newProject = elementBuilder("button", "side-buttons", sideButtons);
     newProject.textContent = "Start New Project";
-    newProject.id = "new-project"
+    newProject.id = "new-project";
   
     const changeView = elementBuilder("button", "side-buttons", sideButtons)
-    changeView.id = "view-button"
-    changeView.textContent = "Change View"
-  
-    const notifHead = elementBuilder("h2", "notif-head", element);
+    changeView.id = "view-button";
+    changeView.textContent = "Change View";
+
+    const barContainer = elementBuilder("div", "bar-container", element);
+    const notifHead = elementBuilder("h2", "notif-head", barContainer);
     notifHead.textContent = "Notifications";
-  
-    const notifications = elementBuilder("div", "notif-bar", element);
-    
-    const currentDate = elementBuilder("p", "today-notif", notifications);
+    const notifications = elementBuilder("div", "notif-bar", barContainer);
+    const currentDate = elementBuilder("p", "notif", notifications);
     currentDate.textContent = today();
-  
     return { element, newProject, changeView, notifHead, notifications, today }
 })();
 
