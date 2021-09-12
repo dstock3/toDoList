5eb4be48df7e8a25dd328c7c658a78be00664176
dstock3 <dstock3@gmail.com>
2021-09-12 13:51:58 -0500

    Moved the changeView function to the buttons module.

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,25 +1,9 @@
 function store(taskObj) {
-  let project = taskObj.project;
-  let projectList = "";
-  for (let i = 0; i < project.taskArray.length; i++) {
-    let task = `${project.taskArray[i]}, `;
-    projectList += task;
-  }
+  /*
+  let projObj = JSON.stringify(taskObj)
+  console.log(projObj) */
+  //let project = taskObj.project;
 
-  let projObj = {
-    title: `${project.title}`,
-    description: `${project.description}`,
-    taskArray: projectList,
-  };
-
-  localStorage.setItem("title", taskObj.title);
-  localStorage.setItem("project", projObj);
-  localStorage.setItem("description", taskObj.description);
-  localStorage.setItem("enteredDate", taskObj.enteredDate);
-  localStorage.setItem("dueDate", taskObj.dueDate);
-  localStorage.setItem("priority", taskObj.priority);
-  localStorage.setItem("notes", taskObj.notes);
-  localStorage.setItem("status", taskObj.status);
 }
 
 export { store };
