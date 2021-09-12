239c34ca338a11470e65ca569be6bae14422d533
dstock3 <dstock3@gmail.com>
2021-09-12 14:24:32 -0500

    Used JSON.parse to retrieve the task object and the project property.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import { project } from "./modules/objectBuilder";
 import { projectBuilder, allProjects } from "./modules/projectCreator";
 import { applyButtons } from "./modules/buttons";
 
-const newTask = localStorage.getItem('newTask');
-console.log(newTask)
 const start = (() => {
-  const defaultProject = project(
-    "New Project",
-    "Add a task below and start tracking your project!",
-    []
-  );
-  const defaultProjectSet = projectBuilder(defaultProject);
-  allProjects.masterList.push(defaultProjectSet);
-  applyButtons(allProjects.masterList);
+  const newTask = JSON.parse(localStorage.getItem('newTask'));
+  let project = JSON.parse(newTask.project);
+  console.log(project)
 
+  if (newTask === null) {
+    const defaultProject = project(
+      "New Project",
+      "Add a task below and start tracking your project!",
+      []
+    );
+    const defaultProjectSet = projectBuilder(defaultProject);
+    allProjects.masterList.push(defaultProjectSet);
+    applyButtons(allProjects.masterList);
+  }
   return { allProjects };
 })();
 
