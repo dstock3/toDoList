5b24c55af604b0f958db001532d37cf9e349effe
David Stockdale <dstock3@gmail.com>
2021-09-18 15:55:27 -0500

    Fixed mistake in nested loop in index.js.

--- src/index.js
+++ src/index.js
@@ -23,22 +23,19 @@ const start = (() => {
     const defaultProjectSet = projectBuilder(defaultProject);
     allProjects.masterList.push(defaultProjectSet);
     applyButtons(allProjects.masterList);
-    
   } else {
     for (let i = 0; i < fetchedList.length; i++) {
       let project = fetchedList[i];
       let newProjSet = projectBuilder(project);
-      
       let tasks = project.taskArray;
       for (let y = 0; y < tasks.length; y++) {
-        notif(tasks[i]);
+        notif(tasks[y]);
       }
       allProjects.masterList.push(newProjSet);
     };
     applyButtons(allProjects.masterList);
     getTheme()
   };
-  
 })();
 
 export { allProjects } 
