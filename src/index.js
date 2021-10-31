c8a575f9f6a9721e3ac1e1f983a0a2e6904aa506
dstock3 <dstock3@gmail.com>
2021-10-31 16:29:32 -0500

    Added 'no new notifications' message in the case of zero projects on start of application.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import "./style.css";
 import { projectTracker, createDefaultProj } from "./modules/objectBuilder";
 import { projectBuilder } from "./modules/projectCreator";
 import { applyButtons, taskButtons } from "./modules/buttons";
-import { notif } from "./modules/sidebar";
+import { notif, notifButton } from "./modules/sidebar";
 import { store, checkList, getTheme } from "./modules/store";
 import { setTheme } from "./modules/themes";
+import { elementBuilder } from "./modules/elements";
 
 window.document.title = "OnTask"
 
@@ -34,6 +35,10 @@ const start = (() => {
     const defaultProjectSet = projectBuilder(defaultProj);
     taskButtons(defaultProjectSet);
     setTheme("classic", "classicComp");
+    let notifBar = document.getElementsByClassName("notif-bar")[0];
+    let noNotif = elementBuilder("div", "notif", notifBar);
+    noNotif.textContent = "No New Notifications"
+    noNotif.id = "no-notif"
   } else {
     buildProjects(fetchedList);
   };
